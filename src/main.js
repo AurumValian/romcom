0eca0421f75d7413624e167525849dc3c4986c55
jobbot <joevarela83@gmail.com>
2020-05-13 21:39:54 -0600

    Add DOM variabless and psuedo code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
+
 // Create variables targetting the relevant DOM elements here 👇
 
-.
+var bookCover = document.querySelector('.cover-image');
+var bookTitle = document.querySelector('.cover-title');
+var tagline1 = document.querySelector('.tagline-1');
+var tagline2 = document.querySelector('.tagline-2');
+
+console.log('This should point to covers and HTML code: ', bookCover);
+
+
+// Make DOM commands
+// define our variables which are 'var covers'
+//
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -17,3 +29,12 @@ var currentCover;
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+// psuedo code create a function called newRandomCover()
+// create an event listener 'load'
+// within function create a new cover with cover Object
+// run getRandomIndex four times in the contructor (1 covers, 1 titles, 2 descriptors);
+// get dom commands to retrieve the classes from index.html
+// reassign based on the value of our new cover
+
+// Idea Create two functions one to pull new cover and the other to display
