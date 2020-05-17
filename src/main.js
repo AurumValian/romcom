d2b35c8a36a279745fdc962d1ea847cbbec2908c
jobbot <joevarela83@gmail.com>
2020-05-16 22:28:31 -0600

    Rearrange class list commands and add input variables

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ var makeYourOwnButton = document.querySelector('.make-new-button');
 var homeView = document.querySelector('.home-view');
 var formView = document.querySelector('.form-view');
 var savedView = document.querySelector('.saved-view');
+var coverInput = document.querySelector('#cover').value;
+var titleInput = document.querySelector('#title').value;
+var tagline1Input = document.querySelector('#descriptor1').value;
+var tagline2Input = document.querySelector('#descriptor2').value;
+
 
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -32,42 +37,44 @@ function newRandomCover() {
   descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
 }
 
-function loadRandomCover() {
+function loadRandomCover() { // works on initial view
   currentCover = newRandomCover();
   bookCover.setAttribute("src", currentCover.cover);
-  bookTitle.innerText = currentCover.title;
   tagline1.innerText = currentCover.tagline1;
   tagline2.innerText = currentCover.tagline2;
+  bookTitle.innerText = currentCover.title;
 }
 
-function clickOnMake() {
-  randomCoverButton.classList.add('hidden');
-  saveCoverButton.classList.add('hidden');
+function clickOnMake() { // Takes you to create your own cover page
   homeView.classList.add('hidden');
   savedView.classList.add('hidden');
   formView.classList.remove('hidden');
   homeButton.classList.remove('hidden');
+  randomCoverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
 }
 
-function clickOnHome() {
-  homeButton.classList.add('hidden');
+function clickOnHome() { // back to home (initial view)
   formView.classList.add('hidden');
   savedView.classList.add('hidden');
   homeView.classList.remove('hidden');
+  homeButton.classList.add('hidden');
   randomCoverButton.classList.remove('hidden');
   saveCoverButton.classList.remove('hidden');
 }
 
-function clickOnSavedCovers() {
-  homeView.classList.add('hidden');
-  randomCoverButton.classList.add('hidden');
-  saveCoverButton.classList.add('hidden');
+function clickOnSavedCovers() { // iteration 4 where we add new titles
   formView.classList.add('hidden');
+  homeView.classList.add('hidden');
+  savedView.classList.remove('hidden');
   homeButton.classList.remove('hidden');
   makeYourOwnButton.classList.remove('hidden');
-  savedView.classList.remove('hidden');
+  randomCoverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
   viewSavedButton.classList.remove('hidden');
 }
+
+function createNewCover()
 
 function makeVisible() {
   homeView.classList.remove('hidden');
